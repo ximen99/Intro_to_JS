39db127e32cd53ff542e48bd84e66af964a4f4a6
ximen99 <877297588@qq.com>
2021-06-13 22:50:59 -0700

    started String and Text Problem Set

--- JS101_Small_Problems/list_processing.js
+++ JS101_Small_Problems/list_processing.js
@@ -63,3 +63,85 @@
 // }
 
 // console.log(substrings("abcde"));
+
+// function isPalindrome(word) {
+//   return (word.length > 1) && (word === word.split("").reverse().join(""));
+// }
+
+// console.log(isPalindrome('aa'))
+
+// function palindromes(str) {
+//   let resultArr = [];
+//   for (let i = 0; i < str.length; i++) {
+//     for (let j = i; j < str.length; j++) {
+//       if (isPalindrome(str.slice(i, j + 1))) {
+//         resultArr.push(str.slice(i, j + 1))
+//       };
+//     }
+//   }
+//   return resultArr;
+// }
+
+// console.log(palindromes('hello-madam-did-madam-goodbye'))
+
+// function sumOfSums(arr) {
+//   let result = 0;
+//   arr.reduce((sum, i) => {
+//     sum += i;
+//     result += sum;
+//     return sum
+//   }, 0);
+//   return result;
+// }
+
+// console.log(sumOfSums([1, 2, 3, 4, 5]));
+
+// function buyFruit(arr) {
+//   let result = [];
+//   for (let i = 0; i < arr.length; i++) {
+//     let j = 1;
+//     while (j <= arr[i][1]) {
+//       result.push(arr[i][0]);
+//       j++;
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(buyFruit([['apple', 3], ['orange', 1], ['banana', 2]]));
+
+// function transactionsFor(id, transactions) {
+//   let result = [];
+//   for (let i = 0; i < transactions.length; i++) {
+//     if (transactions[i].id === id) {
+//       result.push(transactions[i]);
+//     }
+//   }
+//   return result;
+// }
+
+// let transactions = [{ id: 101, movement: 'in', quantity: 5 },
+// { id: 105, movement: 'in', quantity: 10 },
+// { id: 102, movement: 'out', quantity: 17 },
+// { id: 101, movement: 'in', quantity: 12 },
+// { id: 103, movement: 'out', quantity: 20 },
+// { id: 102, movement: 'out', quantity: 15 },
+// { id: 105, movement: 'in', quantity: 25 },
+// { id: 101, movement: 'out', quantity: 18 },
+// { id: 102, movement: 'in', quantity: 22 },
+// { id: 103, movement: 'out', quantity: 15 },];
+
+// // console.log(transactionsFor(101, transactions));
+
+// function isItemAvailable(id, transactions) {
+//   return transactionsFor(id, transactions).reduce((cum, transaction) => {
+//     if (transaction.movement === 'in') {
+//       cum += transaction.quantity;
+//     } else {
+//       cum -= transaction.quantity;
+//     }
+//     return cum
+//   }, 0) > 0;
+// }
+
+// console.log(isItemAvailable(105, transactions));
---
+++ JS101_Small_Problems/string&text.js
@@ -0,0 +1,11 @@
+// function isUpperCase(str) {
+//   return str.toUpperCase() === str;
+// }
+
+// function removeVowels(strArr) {
+//   return strArr.map(word => word.split('').filter(
+//     element => !'aeiouAEIOU'.includes(element)
+//   ).join(''));
+// }
+
+// console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));
