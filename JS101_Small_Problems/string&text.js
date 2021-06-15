b41edae98ed4f7a72ef44093499d15544fe0a9c7
ximen99 <877297588@qq.com>
2021-06-14 22:19:03 -0700

    string commit 2

--- JS101_Small_Problems/string&text.js
+++ JS101_Small_Problems/string&text.js
@@ -9,3 +9,40 @@
 // }
 
 // console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));
+
+// function letterCaseCount(str) {
+//   result = {
+//     lowercase: str.match(/[a-z]/g).length,
+//     uppercase: str.match(/[A-Z]/g).length,
+//     neither: str.match(/[^a-zA-Z]/g).length
+//   };
+//   return result;
+// }
+
+// console.log(letterCaseCount('abCdef 123'));
+
+// function wordCap(str) {
+//   return str.split(' ').map(word => {
+//     return word.replace(word.slice(0, 1), letter => {
+//       if (letter.match(/[a-z]/)) {
+//         return letter.toUpperCase();
+//       } else {
+//         return letter
+//       }
+//     })
+//   }).join(' ');
+// }
+
+// console.log(wordCap('this is a "quoted" word'));
+
+function swapCase(sentence) {
+  return sentence.replace(/[a-z]/ig, letter => {
+    if (letter.match(/[a-z]/)) {
+      return letter.toUpperCase();
+    } else {
+      return letter.toLowerCase();
+    }
+  });
+}
+
+console.log(swapCase('CamelCase'));
