344b473ab319aee5ef0ef423b68c23720a0978b4
ximen99 <877297588@qq.com>
2021-06-21 23:19:43 -0700

    start medium 1

---
+++ JS101_Small_Problems/medium_1.js
@@ -0,0 +1,32 @@
+// function rotateArray(array) {
+//   if (array.length === 0) {
+//     return [];
+//   } else if (!Array.isArray(array)) {
+//     return undefined;
+//   } else {
+//     return array.slice(1, array.length).concat(array.slice(0, 1));
+//   }
+// }
+
+// console.log(rotateArray(3));
+
+// function rotateRightmostDigits(number, digitCount) {
+//   return Number(String(number)
+//     .slice(0, -digitCount)
+//     .concat(rotateArray(String(number)
+//       .slice(-digitCount)
+//       .split(''))
+//       .join('')));
+// }
+
+// console.log(rotateRightmostDigits(735291, 4));
+
+// function maxRotation(number) {
+//   let result = number;
+//   for (let i = String(number).length; i >= 2; i--) {
+//     result = rotateRightmostDigits(result, i);
+//   }
+//   return result;
+// }
+
+// console.log(maxRotation(8703529146));
--- JS101_Small_Problems/string&text.js
+++ JS101_Small_Problems/string&text.js
@@ -35,14 +35,61 @@
 
 // console.log(wordCap('this is a "quoted" word'));
 
-function swapCase(sentence) {
-  return sentence.replace(/[a-z]/ig, letter => {
-    if (letter.match(/[a-z]/)) {
-      return letter.toUpperCase();
-    } else {
-      return letter.toLowerCase();
-    }
-  });
-}
+// function swapCase(sentence) {
+//   return sentence.replace(/[a-z]/ig, letter => {
+//     if (letter.match(/[a-z]/)) {
+//       return letter.toUpperCase();
+//     } else {
+//       return letter.toLowerCase();
+//     }
+//   });
+// }
 
-console.log(swapCase('CamelCase'));
+// console.log(swapCase('CamelCase'));
+
+// function staggeredCase(sentence) {
+//   let charIndex = 0;
+//   return sentence
+//     .split('')
+//     .map(char => {
+//       if (/[a-z]/i.test(char)) {
+//         if (charIndex % 2 === 0) {
+//           charIndex += 1;
+//           return char.toUpperCase();
+//         } else {
+//           charIndex += 1;
+//           return char.toLowerCase();
+//         }
+//       }
+//       return char;
+//     })
+//     .join('');
+// }
+
+// console.log(staggeredCase('ignore 77 the 444 numbers'));
+
+// function wordLengths(sentence) {
+//   if (arguments.length === 0 || sentence.length === 0) {
+//     return [];
+//   }
+
+//   let result = [];
+//   sentence.split(' ')
+//     .forEach(element => {
+//       result.push(`${element} ${element.length}`)
+//     });
+//   return result;
+// }
+
+// console.log(wordLengths("It ain't easy, is it?"));
+
+// function searchWord(keyWord, sentence) {
+//   let regex = new RegExp(keyWord, "ig");
+//   return sentence.replace(regex, `##${keyWord.toUpperCase()}##`);
+// }
+
+// const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
+
+// console.log(searchWord('sed', text));
+
+
